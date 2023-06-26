756887a4442f0719d72599436702a31686738293
Alex Shpyrkovich <aliaksandr.shpyrkovich@insite.ai>
2023-06-26 21:08:54 +0200

    mv command

--- src/index.js
+++ src/index.js
@@ -1,74 +1,75 @@
-import * as readline from 'node:readline';
-import os from 'os';
-import { sep } from 'node:path';
-import { cd, showDir, ls, cat, add, cp } from './scripts/index.js'
-import path from 'node:path';
-import { rn } from './scripts/rn.js';
+import * as readline from "node:readline";
+import os from "os";
+import { sep } from "node:path";
+import { cd, showDir, ls, cat, add, cp, mv } from "./scripts/index.js";
+import path from "node:path";
+import { rn } from "./scripts/rn.js";
 
 const args = process.argv.splice(2);
-const USER_NAME = args[0].split('=')[1]
+const USER_NAME = args[0].split("=")[1];
 
 let currentDir = os.homedir();
 
-
-const greetString = `Welcome to the File Manager, ${USER_NAME}!`
-const exitString = `Thank you for using File Manager, ${USER_NAME}, goodbye!`
+const greetString = `Welcome to the File Manager, ${USER_NAME}!`;
+const exitString = `Thank you for using File Manager, ${USER_NAME}, goodbye!`;
 
 const initExitProcess = () => {
-    console.log(exitString)
-    process.exit(0)
+  console.log(exitString);
+  process.exit(0);
 };
 
 const handleUp = () => {
-    currentDir = path.resolve(currentDir, '..')
-}
+  currentDir = path.resolve(currentDir, "..");
+};
 
-const handleLine = async input => {
-    const [command, ...args] = input.split(" ");
-    switch (command) {
-        case '.exit':
-            initExitProcess();
-            break;
-        case 'up':
-            handleUp();
-            break;
-        case 'cd':
-            currentDir = await cd(currentDir, args[0]) || currentDir;
-            break;
-        case 'ls':
-            ls(currentDir);
-            break;
-        case 'cat':
-            cat(currentDir, args[0]);
-            break;
-        case 'add':
-            add(currentDir, args[0]);
-            break;    
-        case 'rn':
-            rn(currentDir, args);
-            break;
-        case 'cp':
-            cp(currentDir, args);
-            break;                       
-        default:
-            console.log(`Invalid input. Try to write correct command!`);    
-    }
-}
+const handleLine = async (input) => {
+  const [command, ...args] = input.split(" ");
+  switch (command) {
+    case ".exit":
+      initExitProcess();
+      break;
+    case "up":
+      handleUp();
+      break;
+    case "cd":
+      currentDir = (await cd(currentDir, args[0])) || currentDir;
+      break;
+    case "ls":
+      ls(currentDir);
+      break;
+    case "cat":
+      cat(currentDir, args[0]);
+      break;
+    case "add":
+      add(currentDir, args[0]);
+      break;
+    case "rn":
+      rn(currentDir, args);
+      break;
+    case "cp":
+      cp(currentDir, args);
+      break;
+    case "mv":
+      mv(currentDir, args);
+      break;
+    default:
+      console.log(`Invalid input. Try to write correct command!`);
+  }
+};
 
 const runApp = () => {
-    console.log(greetString)
-    showDir(currentDir)
+  console.log(greetString);
+  showDir(currentDir);
 
-    const rl = readline.createInterface(process.stdin, process.stdout)
+  const rl = readline.createInterface(process.stdin, process.stdout);
 
-    rl.on('line', (input) => {
-        handleLine(input);
-    });
+  rl.on("line", (input) => {
+    handleLine(input);
+  });
 
-    rl.on('SIGINT', () => {
-        initExitProcess()
-    });
+  rl.on("SIGINT", () => {
+    initExitProcess();
+  });
+};
 
-}
-
-runApp()
+runApp();
---
+++ src/scripts/mv.js
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import path from 'path';
+import { sep } from 'path';
+import { handleOperationFailed, showDir } from './index.js';
+
+export const mv = async (currDir, [pathToFile, pathToNewDir]) => {
+    const absPathToFile = path.resolve(currDir, pathToFile)
+    const originalFileName = absPathToFile.split(sep).slice(-1);
+    const absPathToNewDir = path.resolve(currDir, `${pathToNewDir + sep + originalFileName}`)
+
+
+    const rs = fs.createReadStream(absPathToFile);
+    const ws = fs.createWriteStream(absPathToNewDir);
+    rs.pipe(ws);
+
+    ws.on('finish', () => {
+        fs.unlink(absPathToFile, (error) => {
+            if (error) handleOperationFailed(error);
+            showDir(currDir);
+        });
+      });
+
+
+    rs.on("error", (error) => {
+        handleOperationFailed(error);
+        showDir(currDir);
+      });
+    
+      ws.on("error", (error) => {
+        handleOperationFailed(error);
+        showDir(currDir);
+      });
+}
